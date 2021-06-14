cdfbdd4d9ca179b8cc8457ba4254b780109b5ad4
HardIk Patel <psrn.hardik@gmail.com>
2021-06-14 16:22:55 +0530

    20210614-1622

--- resources/views/front/layout/styles.blade.php
+++ resources/views/front/layout/styles.blade.php
@@ -2,6 +2,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
 <link href="{{ asset('frontend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('frontend/assets/css/fontawesome.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/assets/css/templatemo-seo-dream.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/assets/css/templatemo.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/assets/css/animated.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/assets/css/owl.css') }}">
