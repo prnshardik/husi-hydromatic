316563c65d96082c4e80dfcc75f76eee18fdeb22
HardIk Patel <psrn.hardik@gmail.com>
2021-06-16 23:48:50 +0530

    20210616-2349

--- resources/views/front/layout/footer.blade.php
+++ resources/views/front/layout/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <p>Copyright © {{ date('Y') }} {{ _site_title() }}. All Rights Reserved. 
+                <p>Copyright © {{ date('Y') }} {{ _settings('SITE_TITLE') }}. All Rights Reserved. 
             </div>
         </div>
     </div>
